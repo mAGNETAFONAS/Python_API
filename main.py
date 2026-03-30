c7b843d8bac1f979adbb3bf6c35abcccd457cb3a
SimasKorolis <simkorol@gmail.com>
2026-03-30 19:37:12 +0300

    feat: Added error handling

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 import http.server
 import os
+import os.path
 import socketserver
-import re
+
 
 cwd = os.getcwd()
 pattern = "\w"
@@ -13,23 +14,27 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, request, client_address, server: socketserver.BaseServer):
         super().__init__(request, client_address, server)
 
+    @property
     def api_response_status(self):
         return "Server is running.".encode()
 
     def api_response_list(self):
-        list_dict = {}
+        list_dict = dict()
         list_dict["Files"] = os.listdir(cwd + self.path)
         return json.dumps(list_dict).encode()
 
     def api_response_file(self):
-        file_dict = {}
-        file_dict["filename"] = self.path.split("/")[1]
-        file_path = cwd + self.path
-        with open(file_path, "r") as file:
-            content = file.readlines()
-            file.close()
-        file_dict["content"] = str(content)
-        return json.dumps(file_dict).encode()
+        if os.path.isfile(cwd + self.path):
+            file_dict = dict()
+            file_dict["filename"] = self.path.split("/")[2]
+            file_path = cwd + self.path
+            with open(file_path, "r") as file:
+                content = file.readlines()
+                file.close()
+            file_dict["content"] = str(content)
+            return json.dumps(file_dict).encode()
+        else:
+            return json.dumps({"Error message" : "File not found."}).encode()
 
     def do_GET(self):
         if self.path == "/":
@@ -37,22 +42,22 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(self.api_response_status())
+            self.wfile.write(self.api_response_status)
 
-        elif self.path == "/files":
-            print(self.path)
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(self.api_response_list())
+        elif self.path.startswith("/files"):
+            if not self.path.startswith("/files/"):
+                print(self.path)
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(self.api_response_list())
+            else:
+                print(self.path)
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(self.api_response_file())
 
-        elif re.match(pattern, self.path.split("/")[1]) != None :
-            print(self.path)
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(self.api_response_file())
-        print(self.path)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8000
