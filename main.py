12e46f999d2749a8cd0b9b7242cdcd6c09dd14c4
SimasKorolis <simkorol@gmail.com>
2026-03-30 15:43:58 +0300

    feat: Added file content reading; JSON output

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import json
 import http.server
 import os
 import socketserver
+import re
 
 cwd = os.getcwd()
+pattern = "\w"
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
@@ -14,23 +16,43 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def api_response_status(self):
         return "Server is running.".encode()
 
-    def api_response_dir(self):
-        return "files: ".encode() + str(os.listdir(cwd + self.path)).encode()
+    def api_response_list(self):
+        list_dict = {}
+        list_dict["Files"] = os.listdir(cwd + self.path)
+        return json.dumps(list_dict).encode()
+
+    def api_response_file(self):
+        file_dict = {}
+        file_dict["filename"] = self.path.split("/")[1]
+        file_path = cwd + self.path
+        with open(file_path, "r") as file:
+            content = file.readlines()
+            file.close()
+        file_dict["content"] = str(content)
+        return json.dumps(file_dict).encode()
 
     def do_GET(self):
         if self.path == "/":
             print(self.path)
             self.send_response(200)
-            self.send_header("Content-type", "text/plain ")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(self.api_response_status())
 
-        if self.path == "/files":
+        elif self.path == "/files":
             print(self.path)
             self.send_response(200)
-            self.send_header("Content-type", "text/plain ")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(self.api_response_dir())
+            self.wfile.write(self.api_response_list())
+
+        elif re.match(pattern, self.path.split("/")[1]) != None :
+            print(self.path)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(self.api_response_file())
+        print(self.path)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8000
