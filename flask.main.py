1bf9514e9db5dc06c24580126de403e171411219
SimasKorolis <simkorol@gmail.com>
2026-04-01 12:48:53 +0300

    refactor: expanded on external config

--- flask.main.py
+++ flask.main.py
@@ -3,11 +3,17 @@ from flask import request
 import json
 import os
 import yaml
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 cwd = os.getcwd()
+
+# Using 2 different ways of externalizing settings for learning purposes
 config = yaml.safe_load(open(f"{cwd}/config.yml"))
 allowed_directories = config["Permission"]["directories"]
+
+load_dotenv()
+allowed_dir_env = os.getenv("ALLOWED_DIR")
 
 @app.route("/", methods = ["GET"])# API response for successful startup
 def home():
@@ -15,29 +21,37 @@ def home():
 
 @app.route("/<dir_name>", methods = ["GET"])# API response for listing directory content
 def get_dir(dir_name):
-    if os.path.isdir(cwd + request.path):
+    dir_path = cwd + "/" + dir_name
+    if os.path.isdir(dir_path):
         if dir_name in allowed_directories:
             list_dict = dict()
-            list_dict["Files"] = os.listdir(cwd + request.path)
+            list_dict["Files"] = os.listdir(dir_path)
             return json.dumps(list_dict)
         else:
             return json.dumps({"Error message": "No permission"})
     else:
         return json.dumps({"Error message": "Directory not found"})
 
-@app.route("/files/<filename>", methods = ["GET"])# API response for listing file content
-def get_file(filename):
-    if os.path.isfile(cwd + request.path):
-        file_dict = dict()
-        file_dict["filename"] = filename
-        file_path = cwd + request.path
-        with open(file_path, "r") as file:
-            content = file.readlines()
-            file.close()
-        file_dict["content"] = str(content)
-        return json.dumps(file_dict)
+@app.route("/<dir_name>/<filename>", methods = ["GET"])# API response for listing file content
+def get_file(dir_name, filename):
+    file_path = cwd + "/" + dir_name + "/" + filename
+    dir_path = cwd + "/" + dir_name
+    if os.path.isdir(dir_path):
+        if dir_name in allowed_directories:
+            if os.path.isfile(file_path):
+                file_dict = dict()
+                file_dict["filename"] = filename
+                with open(file_path, "r") as file:
+                    content = file.readlines()
+                    file.close()
+                file_dict["content"] = str(content)
+                return json.dumps(file_dict)
+            else:
+                return json.dumps({"Error message": "File not found."})
+        else:
+            return json.dumps({"Error message": "No permission"})
     else:
-        return json.dumps({"Error message": "File not found."})
+        return json.dumps({"Error message": "Directory not found"})
 
 
 if __name__ == "__main__":# Server startup
