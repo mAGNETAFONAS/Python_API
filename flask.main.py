65b4168185c8181f5eaa52a214e6f3e4933b10dc
SimasKorolis <simkorol@gmail.com>
2026-04-02 10:18:31 +0300

    feat: Added logging

--- flask.main.py
+++ flask.main.py
@@ -1,12 +1,19 @@
 from flask import Flask
-from flask import request
+import datetime
+from http import HTTPStatus
 import json
+import logging
 import os
 import yaml
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 cwd = os.getcwd()
+date = datetime.datetime.now()
+
+logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Using 2 different ways of externalizing settings for learning purposes
 config = yaml.safe_load(open(f"{cwd}/config.yml"))
@@ -17,25 +24,35 @@ allowed_dir_env = os.getenv("ALLOWED_DIR")
 
 @app.route("/", methods = ["GET"])# API response for successful startup
 def home():
+    logging.info("User requested: /")
+    logging.info("Request successful, code: %d", HTTPStatus.OK.value)
     return json.dumps({"message":"Server is running."})
 
 @app.route("/<dir_name>", methods = ["GET"])# API response for listing directory content
 def get_dir(dir_name):
-    dir_path = cwd + "/" + dir_name
+    dir_path = os.path.join(cwd, dir_name)
     if os.path.isdir(dir_path):
         if dir_name in allowed_directories:
             list_dict = dict()
             list_dict["Files"] = os.listdir(dir_path)
+            #print(f"INFO: At {date} user requested {dir_name}")
+            #print(f"INFO: request was successful")
+            logging.info("User requested: %s", dir_name)
+            logging.info("Request successful, code: %d", HTTPStatus.OK.value)
             return json.dumps(list_dict)
         else:
-            return json.dumps({"Error message": "No permission"})
+            logging.info("User requested: %s", dir_name)
+            logging.warning("Request unsuccessful, code: %d", HTTPStatus.FORBIDDEN.value)
+            return json.dumps({"Error message": "No permission"}), HTTPStatus.FORBIDDEN.value
     else:
-        return json.dumps({"Error message": "Directory not found"})
+        logging.info("User requested: %s", dir_name)
+        logging.warning("Request unsuccessful, code: %d", HTTPStatus.NOT_FOUND.value)
+        return json.dumps({"Error message": "Directory not found"}), HTTPStatus.NOT_FOUND.value
 
 @app.route("/<dir_name>/<filename>", methods = ["GET"])# API response for listing file content
 def get_file(dir_name, filename):
-    file_path = cwd + "/" + dir_name + "/" + filename
-    dir_path = cwd + "/" + dir_name
+    file_path = os.path.join(cwd, dir_name, filename)
+    dir_path = os.path.join(cwd, dir_name)
     if os.path.isdir(dir_path):
         if dir_name in allowed_directories:
             if os.path.isfile(file_path):
@@ -45,13 +62,21 @@ def get_file(dir_name, filename):
                     content = file.readlines()
                     file.close()
                 file_dict["content"] = str(content)
+                logging.info("User requested: %s", dir_name)
+                logging.info("Request successful, code: %d", HTTPStatus.OK.value)
                 return json.dumps(file_dict)
             else:
-                return json.dumps({"Error message": "File not found."})
+                logging.info("User requested: %s", filename)
+                logging.warning("Request unsuccessful, code: %d", HTTPStatus.NOT_FOUND.value)
+                return json.dumps({"Error message": "File not found."}), HTTPStatus.NOT_FOUND.value
         else:
-            return json.dumps({"Error message": "No permission"})
+            logging.info("User requested: %s", dir_name)
+            logging.warning("Request unsuccessful, code: %d", HTTPStatus.FORBIDDEN.value)
+            return json.dumps({"Error message": "No permission"}), HTTPStatus.FORBIDDEN.value
     else:
-        return json.dumps({"Error message": "Directory not found"})
+        logging.info("User requested: %s", dir_name)
+        logging.warning("Request unsuccessful, code: %d", HTTPStatus.NOT_FOUND.value)
+        return json.dumps({"Error message": "Directory not found"}), HTTPStatus.NOT_FOUND.value
 
 
 if __name__ == "__main__":# Server startup
