5ca51f893fd7e56dcdf223a2240fe89614174f25
SimasKorolis <simkorol@gmail.com>
2026-04-09 16:07:57 +0300

    refactor: Rewrote Dockerfile and scripts to use bind mounting instead of copying everything

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM python:latest
+FROM python:3.13.5
 WORKDIR /app
 
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
-#COPY flask.main.py /app
-#COPY config.yml /app
-COPY . /app
+COPY /templates/ /app/templates
+COPY config.yml ./
+COPY flask.main.py ./
 
 EXPOSE 8000
 CMD ["python3", "flask.main.py"]
--- start_docker.sh
+++ start_docker.sh
@@ -8,4 +8,6 @@ container_port=8000
 docker stop $container_name 2>/dev/null
 docker rm $container_name 2>/dev/null
 
-docker run --name "$container_name" -it -d -p $host_port:$container_port "$image_name"
+docker run --name "$container_name" -it -d \
+--mount type=bind,source="$(pwd)"/files/,target=/app/files/,readonly \
+-p $host_port:$container_port "$image_name"
