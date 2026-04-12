da7b4c546627f26e3d4c92761487f07a0b2f2d1f
SimasKorolis <simkorol@gmail.com>
2026-04-12 16:40:17 +0300

    feat: Added further flag usage to all scripts

--- start_docker.sh
+++ start_docker.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
-image_name=$1
-container_name=$2
-host_port=8000
-container_port=8000
+help() {
+  echo "Usage: $0 [OPTIONS] [IMAGE NAME] [CONTAINER NAME] [HOST PORT] [CONTAINER PORT]"
+  echo "Options:"
+  echo "-h, --help     Display help message"
+}
 
-docker stop $container_name 2>/dev/null
-docker rm $container_name 2>/dev/null
-
-docker run --name "$container_name" -it -d \
---mount type=bind,source="$(pwd)"/files/,target=/app/files/,readonly \
--p $host_port:$container_port "$image_name"
+  if [ $# -eq 1 ]; then
+    case $1 in
+      -h | --help)
+        help
+        exit 0
+        ;;
+      *)
+        echo "ERROR: invalid number of arguments"
+        help
+        ;;
+    esac
+  else :
+    if [ $# -eq 4 ]; then
+        docker container ls -a | grep -w "$2" > /dev/null 2>&1
+        if [[ $? = 0 ]]; then
+          echo "Container with this name already exists."
+          exit 0
+        else
+          docker run --name "$2" -it -d \
+          --mount type=bind,source="$(pwd)"/files/,target=/app/files/,readonly \
+          -p "$3:$4" "$1"
+        fi
+    elif [ $# -eq 0 ]; then
+        echo "ERROR: No argument given"
+        help
+        exit 0
+    elif [ $# -lt 4 ]; then
+        echo "ERROR: Not enough arguments"
+        help
+        exit 0
+    else
+        echo "ERROR: Too many arguments"
+        help
+        exit 0
+    fi
+  fi
--- stop.sh
+++ stop.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-function prompt() {
-  clear
-  echo "Do you want to: "
-  echo "1. Only stop the container"
-  echo "2. Stop and remove the container"
-  echo "3. Exit"
-  read command
+help() {
+  echo "Usage: $0 [OPTIONS]"
+  echo "Options:"
+  echo "-h, --help       Display help message"
+  echo "-rm, --remove     Stop and remove docker compose containers"
 }
 
-prompt
 
-while [[ $command -ne 3 ]]; do
-  case $command in
-    1)
-      docker compose stop
-      ;;
-    2)
-      docker compose down
-      ;;
-  esac
-  prompt
-done
+  if [ $# -gt 0 ]; then
+    case $1 in
+      -h | --help)
+        help
+        exit 0
+        ;;
+      -rm | --remove)
+        docker compose down
+        ;;
+      *)
+        echo "Invalid argument"
+        help
+        exit 0
+        ;;
+    esac
+  else :
+    docker compose stop
+  fi
 
