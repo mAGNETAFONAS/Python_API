9fefc5c3e2d67f64bf4c5434d54097090cde5aa3
SimasKorolis <simkorol@gmail.com>
2026-04-20 13:05:52 +0300

    refactor: changed database functionality to come from class functions

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,5 @@ export COMPOSE_VERSION=$(cat VERSION.md)
 image_name=python_api_web
 
 docker compose -f ./compose.yaml up -d --build
-
+docker image rm $image_name:latest
 docker tag $image_name:$COMPOSE_VERSION $image_name:latest
