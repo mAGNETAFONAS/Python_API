b6d910afda832905e657d7284dca0e3975af9e42
SimasKorolis <simkorol@gmail.com>
2026-04-22 11:15:45 +0300

    chore: deleted files

--- scripts/build_comp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export COMPOSE_VERSION=$(cat VERSION.md)
-image_name=python-api-web
-
-docker compose -f ./compose.yaml up -d --build
-docker image rm $image_name:latest
-docker tag $image_name:$COMPOSE_VERSION $image_name:latest
