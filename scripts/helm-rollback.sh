c9ae2b9a4ec51ddab944cd9f346e04dfeb301fba
SimasKorolis <simkorol@gmail.com>
2026-05-12 15:06:37 +0300

    feat: added scripts for Helm upgrade and rollback

---
+++ scripts/helm-rollback.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+help() {
+  echo "Usage: $0 [RELEASE NAME] [REVISION] [OPTIONS] "
+  echo "Options:"
+  echo "-h, --help       Display help message"
+}
+
+
+  if [ $# -eq 1 ]; then
+    case $1 in
+      -h | --help)
+        help
+        exit 0
+        ;;
+      *)
+        echo "ERROR: Not enough arguments!"
+        help
+        ;;
+    esac
+  elif  [ $# -eq 2 ]; then
+          helm list | grep -w "$1" > /dev/null 2>&1
+          if [[ $? = 0 ]]; then
+            helm rollback "$1" "$2" \
+              --wait \
+              --timeout 5m
+          else
+            echo "ERROR: chart does not exist."
+            help
+        fi
+  elif [ $# -eq 3 ]; then
+    help
+  elif [ $# -eq 0 ]; then
+    echo "ERROR: No argument given"
+    help
+    exit 0
+  elif [ $# -gt 3 ]; then
+    echo "ERROR: Too many arguments"
+    help
+    exit 0
+
+
+  fi
